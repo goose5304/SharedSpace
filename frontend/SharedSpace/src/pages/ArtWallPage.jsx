e66913fe6d3afc02244cc0ee9c8913362c11d247
Nathaniel Escano <205729250+Natex15@users.noreply.github.com>
2026-01-07 17:35:57 +0800

    chore: add some css to artwallPage

--- frontend/SharedSpace/src/pages/ArtWallPage.jsx
+++ frontend/SharedSpace/src/pages/ArtWallPage.jsx
@@ -1,18 +1,20 @@
 import './ArtWallPage.css'
-import SampleImg from '../assets/react.svg'
-
+import SampleImg from '../assets/SharedSpaceLogo.svg'
+import SampleImg2 from '../assets/react.svg'
 export function ArtWallPage(){
-    const artWorks = [SampleImg,SampleImg,SampleImg,SampleImg];
+    const artWorks = [SampleImg,SampleImg2,SampleImg,SampleImg,SampleImg,SampleImg2,SampleImg,SampleImg,SampleImg2];
     return(
         <div className="artWallContainer">
             <h1 className='text'>Art Wall</h1>
             <p className='text'>See what the community’s been creating lately 🎨</p>
-            <div className='artWall'>
-                {
-                artWorks.map((art,index) => (
-                    <img src={art} alt='Image' key={index} className='artWork' />
-                ))
-                }
+            <div className='artWallWrapper'>
+                <div className='artWall'>
+                    {
+                    artWorks.map((art,index) => (
+                        <img src={art} alt='Image' key={index} className='artWork' />
+                    ))
+                    }
+                </div>
             </div>
         </div>
     )
